afef257e72b9f2c93a2aebf3551c140924a3e042
Muhammed Ijlan <ijlanijlu580@gmail.com>
2024-03-13 12:34:41 +0530

    updates

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 const config = {
-    firebase : {
-          apiKey: process.env.REACT_APP_APIKEY,
-          authDomain: process.env.REACT_APP_AUTHDOMAIN,
-          databaseURL: process.env.REACT_APP_DATABASEURL,
-          projectId: process.env.REACT_APP_PROJECTID,
-          storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-          messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-          appId: process.env.REACT_APP_APPID,
-          measurementId: process.env.REACT_APP_MEASUREMENTID,
+    firebase: {
+        apiKey: process.env.REACT_APP_APIKEY,
+        authDomain: process.env.REACT_APP_AUTHDOMAIN,
+        databaseURL: process.env.REACT_APP_DATABASEURL,
+        projectId: process.env.REACT_APP_PROJECTID,
+        storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+        appId: process.env.REACT_APP_APPID,
+        measurementId: process.env.REACT_APP_MEASUREMENTID,
     },
     google: {
         API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
@@ -19,4 +19,4 @@ const config = {
     },
 }
 
-export default config;
+export default config; 
