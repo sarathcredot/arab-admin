ecc97d7fac4295528c5a54a23f7b99a9a2f80776
Riyas Credot <riyas@credot.in>
2025-02-07 12:36:33 +0530

    Return Image swiper removed
    Return Image swiper removed

--- src/components/orders/OrderProductDetails.tsx
+++ src/components/orders/OrderProductDetails.tsx
@@ -1529,7 +1529,7 @@ function OrderProductDetails({
                           <div
                             key={el?._id}
                             className={styles.returnImageContainer}
-                            onClick={toggleReturnImageSwiperModal}
+                            // onClick={toggleReturnImageSwiperModal}
                           >
                             <img
                               key={el?._id}
